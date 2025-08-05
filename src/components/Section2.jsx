81b207aae6784b3abf314494aa5bc5c730b75cc8
Navinraj2405 <anavinraj1100@gmail.com>
2025-08-05 19:40:14 +0530

    Initial commit

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -4,8 +4,8 @@ function Section2(){
         <h1 className="font-bold text-3xl">MacBook Air</h1>
     <p className="text-2xl font-semibold">Sky Blue Color</p>
         <p className="text-2xl font-semibold">Sky High Perfimance with M4</p>
-    <div><button className="bg-blue-500 px-2 py-2 rounded-xl mt-3 text-white" >Learn More</button>
-    <button className="py-2 px-6 rounded-xl mt-3 text-white bg-blue-500 ml-5">Buy</button></div>
+    <div><button className="bg-blue-600 px-2 py-2 rounded-xl mt-3 text-white" >Learn More</button>
+    <button className="py-2 px-6 rounded-xl mt-3 text-white bg-blue-600 ml-5">Buy</button></div>
         <img
         src="/mac.webp"
         alt="mac.webp"
