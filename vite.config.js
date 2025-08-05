6bd97f0d1ac0592ebe3f7d0f8f31590f9c944879
Navinraj2405 <anavinraj1100@gmail.com>
2025-08-05 22:39:02 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/Apple-clone/',
+    base: '/apple-clone-ui/'
 })
  
